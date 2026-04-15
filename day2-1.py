7212613730167fa1aaf7c5c9e01d50831104102b
divineone666 <divine_one@126.com>
2026-04-15 21:47:03 +0800

    Add files via upload
    day2

---
+++ day2-1.py
@@ -0,0 +1,24 @@
+# 写一个数据类型
+type(666)
+print(type(666))
+#这是一个注释
+num=type(666)
+print(num)
+# 写一个类型为浮点数和字符串
+num1=type(13.14)
+print(num1)
+num2=type("aaa")
+print(num2)
+# 数据转换
+"""
+1.整数转小数
+2.小数转整数
+3.整数或小数转字数串"""
+x=10
+z=13.14
+nu=float(x)
+print(type(nu),nu) 
+nu2=int(z)
+print(type(nu2),nu2)
+nu3=str(x)
+print(type(nu3),nu3)
