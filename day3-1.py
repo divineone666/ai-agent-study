57130fa70ce3eeeb1001ee24eaff011a6b2bbbe5
divineone666 <divine_one@126.com>
2026-04-15 21:47:42 +0800

    Add files via upload
    day3 学习

---
+++ day3-1.py
@@ -0,0 +1,51 @@
+# 标识符 只能数字 字母 下划线 中文组成
+# 数字不能在最前面
+# 区分大小写
+# 通用词不能作为标识符 如class
+name=("j)obs")
+print(name)
+# 运算符
+num = 1+1
+print(num)
+num = 6*6
+print(num)
+num = 10-1
+print(num)
+num =10/1
+print(num)
+num=10%3
+num=10//3
+print(num)
+print(num)
+num=2**8
+print(num)
+num=1
+num+=1
+print(num)
+num=2
+num-=2
+print(num)
+num=3
+num*=3
+print(num)
+num=6
+num/=6
+print(num)
+# 字符串的三种定义
+name = ("字符串")
+name_1=('字符串')
+name_2=("""字符串""")
+print(name)
+print(name_1)
+print(name_2)
+name ='"aaa"'
+print(name)
+name ="'aaa'"
+print(name)
+name="\"aaa\""
+print(name)
+name ='\'aaa\''
+print(name)
+# 字符串拼接
+name = "字符串"
+print("这个是"+name+"拼接")
